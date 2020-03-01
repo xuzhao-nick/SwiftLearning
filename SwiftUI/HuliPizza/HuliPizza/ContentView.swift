1cc2f867b5abab02b347d47c69ff5d53d8ff8717
Nick Xu <brainstudiosg@gmail.com>
2020-03-01 20:52:08 +0800

    Setup HuliPizza project

---
+++ SwiftUI/HuliPizza/HuliPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Nick Xu on 1/3/20.
+//  Copyright © 2020 Nick Xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
