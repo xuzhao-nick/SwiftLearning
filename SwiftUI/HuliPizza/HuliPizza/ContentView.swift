1de2d44cd851fe5403391ed6a5e66161fe6f541e
Nick Xu <brainstudiosg@gmail.com>
2020-03-02 12:38:48 +0800

    Update ContentView

--- SwiftUI/HuliPizza/HuliPizza/ContentView.swift
+++ SwiftUI/HuliPizza/HuliPizza/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Huli Pizza Company")
+            Text("Order Pizza")
+        }
     }
 }
 
