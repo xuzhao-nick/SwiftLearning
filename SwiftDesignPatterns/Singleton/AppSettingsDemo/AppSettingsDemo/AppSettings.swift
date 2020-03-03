8d1027ba6f3dbeda555197ece8bc80061c315403
Nick Xu <brainstudiosg@gmail.com>
2020-03-03 23:31:54 +0800

    Add AppSettings

---
+++ SwiftDesignPatterns/Singleton/AppSettingsDemo/AppSettingsDemo/AppSettings.swift
@@ -0,0 +1,14 @@
+//
+//  AppSettings.swift
+//  AppSettingsDemo
+//
+//  Created by Nick Xu on 3/3/20.
+//  Copyright © 2020 Karoly Nyisztor. All rights reserved.
+//
+
+import Foundation
+
+final public class AppSettings {
+    private var settings: [String: Any] = ["THeme": "Dark"]
+    
+}
