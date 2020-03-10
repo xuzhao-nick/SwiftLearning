527d13bf8aea6a3f12f036d1d7f67180123f3f35
Nick Xu <brainstudiosg@gmail.com>
2020-03-10 16:11:32 +0800

    Add shadow and clipShape effect to image

--- SwiftUI/HuliPizza/HuliPizza/MenuView/MenuRowView.swift
+++ SwiftUI/HuliPizza/HuliPizza/MenuView/MenuRowView.swift
@@ -12,6 +12,11 @@ struct MenuRowView: View {
     var body: some View {
         HStack(alignment: .top, spacing: 15) {
             Image("1_100w")
+                // .cornerRadius(10)
+                //.border(Color("G4"),width:2)
+                .clipShape(Capsule())
+                .shadow(color: Color.black.opacity(0.5), radius: 5, x: 5, y: 5)
+           
             VStack(alignment: .leading) {
                 Text("Huli Chicken Pizza")
                     .font(.title)
